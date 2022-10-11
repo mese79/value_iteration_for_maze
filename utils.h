e36bfb7ef1f0d3978f84d7de2f9d58b15378e61c
mese79 <mehdiseifi@gmail.com>
2022-10-11 16:18:38 +0330

    handle flipping vector rows; since start is bottom-left

--- utils.h
+++ utils.h
@@ -5,22 +5,36 @@
 using namespace std;
 
 
-void print_2d_array(auto &data2d) {
-    ostringstream ssRow;
+template <typename T>
+void print_vector2d(vector<vector<T>> &data2d, bool flipY=true) {
     string output;
+    ostringstream ssRow;
     int minPad = 2;
+    // get sample entity length:
     ssRow << data2d[0][0];
     int baseLen = ssRow.str().length() < 8 ? 9 : ssRow.str().length() + 1;
     ssRow.str(string());  // clear row buffer
 
-    for (auto &row: data2d) {
-        for (auto &entity: row) {
-            ssRow << "|" << setw(baseLen + minPad / 2) << entity << string(minPad / 2, ' ');
+    // row iterator: if bottom-left is the (0, 0) then flip rows.
+    auto fromIt = flipY ? data2d.end() : data2d.begin();
+    auto toIt = flipY ? data2d.begin() : data2d.end();
+
+    for (auto rowIt = fromIt; rowIt != toIt;) {
+        if (flipY) {
+            --rowIt;
+        }
+
+        for (T &entity: *rowIt) {
+            ssRow << "|" << setw(baseLen + minPad / 2) << setprecision(5) << entity << string(minPad / 2, ' ');
         }
         ssRow << "|\n";
         output += ssRow.str() + string(ssRow.str().length() - 1, '-') + "\n";
         ssRow.str(string());  // clear row buffer
+
+        if (!flipY) {
+            ++rowIt;
+        }
     }
     output = string(output.find("\n"), '-') + "\n" + output;
     cout << endl << output;
-}
+};
